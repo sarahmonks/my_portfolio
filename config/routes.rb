37498f7c203073e7101d4b204af0d0de5ff7be57
Sarah Monks <monks.sarah@gmail.com>
2017-07-04 20:56:35 +0100

    fixed formatting in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get 'sticky_notes/index'
+    get 'sticky_notes/index'
+    get 'contacts/new'
+    get 'pages/index'
+    get 'pages/skills'
+    get 'pages/works'
+    root 'pages#index'
 
-  get 'contacts/new'
+    get 'contacts', to: 'contacts#new', as: 'new_message'
+    get 'pages', to: 'pages#index'
 
-  get 'pages/index'
-
-  get 'pages/skills'
-
-  get 'pages/works'
-  root 'pages#index'
-
-get 'contacts', to: 'contacts#new', as: 'new_message'
-get 'pages', to: 'pages#index'
-
-resources :sticky_notes
-resources :contacts
+    resources :sticky_notes
+    resources :contacts
 end
