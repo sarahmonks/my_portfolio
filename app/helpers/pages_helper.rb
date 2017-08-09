95e0472fe0fb70cd6bd693901142f949943197c6
Sarah Monks <monks.sarah@gmail.com>
2017-08-09 12:02:40 +0100

    Added azula array in helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -18,13 +18,11 @@ module PagesHelper
 	end
 
 	def azula_animations
-		azula_animations = [{'img_title' => 'Home screen', 'img_src' => "dublin_inquirer1.png"}, 
-							{'img_title' => 'Read section', 'img_src' => "dublin_inquirer2.png"}, 
-							{'img_title' => 'News story', 'img_src' => "dublin_inquirer3.png"},
-							{'img_title' => 'Comments section', 'img_src' => "dublin_inquirer4.png"}, 
-							{'img_title' => 'Side menu', 'img_src' => "dublin_inquirer5.png"},
-							{'img_title' => 'Engage section', 'img_src' => "dublin_inquirer6.png"},
-							{'img_title' => 'Contribution section', 'img_src' => "dublin_inquirer7.png"},
-							{'img_title' => 'User account', 'img_src' => "dublin_inquirer8.png"}]
+		azula_animations = [{'img_alt' => 'Azula waving', 'img_src' => "azula_wave.png", 'width' => '278', 'height' => '264'}, 
+							{'img_alt' => 'Azula puzzled', 'img_src' => "azula_puzzled.png", 'width' => '305', 'height' => '261'}, 
+							{'img_alt' => 'Azula laughing', 'img_src' => "azula_laugh.png",'width' => '278', 'height' => '269'},
+							{'img_alt' => 'Azula dancing', 'img_src' => "azula_dance.png", 'width' => '278', 'height' => '269'}, 
+							{'img_alt' => 'Azula crying', 'img_src' => "azula_sad.png", 'width' => '278', 'height' => '264'},
+							{'img_alt' => 'Azula zipping her mouth', 'img_src' => "azula_zip_it.png", 'width' => '278', 'height' => '269'}]
 	end
 end
